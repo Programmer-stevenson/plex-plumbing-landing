1c6aa5ffac31944bf10517c2bab22ff72264620d
Programmer-stevenson <programmwe.brandon.s@gmail.com>
2026-01-30 12:12:28 -0800

    logo fixed

--- src/App.jsx
+++ src/App.jsx
@@ -366,7 +366,7 @@ const AnimatedLogo = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.3 }}
         >
-          Digital Marketing • Web Design • Brand Strategy
+          Digital Marketing • Web Design • Strategy
         </motion.div>
       </div>
     </motion.div>
